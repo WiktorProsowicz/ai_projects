8d25297da6dfff76f67d61a51c3618338aa6569e
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-08-06 20:50:32 +0200

    General project changes

--- project_setup.bash
+++ project_setup.bash
@@ -8,8 +8,8 @@ function build_project()
     cd "${PROJECT_HOME}/build" || return;
     make -j $(( $(grep -c ^processor /proc/cpuinfo) / 2 ));
     cd ..;
-    cppcheck --enable=all --project=build/compile_commands.json --check-config -iForeignModules/* --std=c++11 --suppress=missingIncludeSystem -ibuild/* .;
-
+    cppcheck --enable=all --project=build/compile_commands.json --check-config -iForeignModules/* --std=c++20 --suppress=missingIncludeSystem -ibuild/* .;
+    
 }
 
 function rebuild_project()
@@ -23,23 +23,32 @@ function rebuild_project()
 function run_clang_tidy()
 {
     find "${PROJECT_HOME}" \
-        \( -name "*.cpp" -o -name "*.c" -o -name "*.cc" \) \
-        -not \( -path "${PROJECT_HOME}/ForeignModules/*" -prune \) \
-        -not \( -path "${PROJECT_HOME}/build/*" -prune \) \
-        -exec clang-tidy -p "${PROJECT_HOME}/build" -config-file="${PROJECT_HOME}/.clang-tidy" -extra-arg=-std=c++20 -header-filter=".*" {} \;
-
+    \( -name "*.cpp" -o -name "*.c" -o -name "*.cc" \) \
+    -not \( -path "${PROJECT_HOME}/ForeignModules/*" -prune \) \
+    -not \( -path "${PROJECT_HOME}/build/*" -prune \) \
+    -exec clang-tidy -p "${PROJECT_HOME}/build" -config-file="${PROJECT_HOME}/.clang-tidy" -extra-arg=-std=c++20 -header-filter=".*" {} \;
+    
 }
 
 function apply_clang_format()
 {
     find "${PROJECT_HOME}" \
-        \( -name "*.cpp" -o -name "*.c" -o -name "*.cc" \) \
-        -not \( -path "${PROJECT_HOME}/ForeignModules/*" -prune \) \
-        -not \( -path "${PROJECT_HOME}/build/*" -prune \) \
-        -exec clang-format -i {} \;
+    \( -name "*.cpp" -o -name "*.c" -o -name "*.cc" \) \
+    -not \( -path "${PROJECT_HOME}/ForeignModules/*" -prune \) \
+    -not \( -path "${PROJECT_HOME}/build/*" -prune \) \
+    -exec clang-format -i {} \;
 }
 
 function run_tests()
 {
-    ctest --test-dir "${PROJECT_HOME}/build" --output-on-failure 
+    ctest --test-dir "${PROJECT_HOME}/build" --output-on-failure
+}
+
+function download_dependencies
+{
+    cd "${PROJECT_HOME}"/ForeignModules || return
+    
+    git clone --recursive "git@github.com:google/googletest.git"
+    
+    cd ..
 }
