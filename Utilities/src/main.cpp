dc2209c1b0961721a0ba41f40edc382cf80eee54
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-09-19 00:42:33 +0200

    Add Utilities executable

---
+++ Utilities/src/main.cpp
@@ -0,0 +1,15 @@
+/***************************************
+ * Main executable for 'Utilities' lib.
+ * 
+ * Copyright (c) 2023
+ * 
+ * by Wiktor Prosowicz
+ ***************************************/
+
+// __C++ standard headers__
+
+// __Own software headers__
+#include <Utilities/BinarySerialization.h>
+#include <Utilities/ThreadPool.h>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* const argv) { }
