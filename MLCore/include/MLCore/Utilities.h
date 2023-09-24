d219765a10334977f1dc4dc1e9e1e27cc695d6da
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2023-09-24 20:33:51 +0200

    Add makeTensor to TensorOperations

--- MLCore/include/MLCore/Utilities.h
+++ MLCore/include/MLCore/Utilities.h
@@ -1,21 +1,72 @@
 #ifndef MLCORE_UTILITIES_H
 #define MLCORE_UTILITIES_H
 
+// __C++ standard headers__
 #include <iterator>
 #include <limits>
 #include <sstream>
 #include <vector>
+#include <variant>
 
+// __External software headers__
 #include <fmt/format.h>
 
 namespace mlCore
 {
 
+namespace detail
+{
+template <typename ContainedType>
+class RawTensorInitList;
+
+/**
+ * @brief Variant used for making compile-time "tensor literals".
+ * 
+ * Usage of the RawTensorForm mimics the numpy.array function i.e tensor = np.array([[1, 2, 3], [4, 5, 6]]).
+ * 
+ * @tparam BaseType Indivisible underlying type of the desired tensor.
+ * 
+ */
+template <typename BaseType>
+using RawTensorForm = std::variant<BaseType, detail::RawTensorInitList<BaseType>>;
+
+/**
+ * @brief Container-type tensor form for nested objects.
+ * 
+ * @tparam ContainedType Underlying object's type held by the initializer list. 
+ * Can be either the destination tensor's data type or another init list. 
+ */
+template <typename ContainedType>
+class RawTensorInitList : public std::vector<RawTensorForm<ContainedType>>
+{
+public:
+	using std::vector<RawTensorForm<ContainedType>>::vector;
+};
+} // namespace detail
+
+/**
+ * @brief Creates a human-readable serialized form of the vector. Can be used for displaying tensors' shapes etc.
+ * 
+ * @param vect Vector to be serialized.
+ * @param openSign Character serving as the beginning for the result sequence.
+ * @param closeSign Character serving as the end for the result sequence.
+ * @return Serialized representation of the vector.
+ * 
+ * @example
+ * 
+ * stringifyVector(std::vector<uint32_t>{0, 1, 2, 3}, '[', ']') -> (0, 1, 2, 3)
+ */
 template <typename T>
 std::string stringifyVector(const std::vector<T>& vect, const char* const openSign = "(", const char* const closeSign = ")")
 {
 	return fmt::format("{}{}{}", openSign, fmt::join(vect, ", "), closeSign);
 }
+
+template <typename BaseType>
+using TensorForm = detail::RawTensorForm<BaseType>;
+
+template <typename BaseType>
+using TensorArr = detail::RawTensorInitList<BaseType>;
 
 } // namespace mlCore
 
--- MLCore/src/MLCore/TensorOperations.cpp
+++ MLCore/src/MLCore/TensorOperations.cpp
@@ -1,6 +1,11 @@
+// __Related headers__
 #include <MLCore/TensorOperations.h>
-#include <MLCore/TensorOperationsImpl.h>
+
+// __C++ standard headers__
 #include <cmath>
+
+// __Own software headers__
+#include <MLCore/TensorOperationsImpl.h>
 
 namespace mlCore
 {
@@ -46,5 +51,103 @@ BasicTensor<ValueType> BasicTensorOperations<ValueType>::sigmoid(const BasicTens
 		val = 1.0 / (1.0 + std::pow(M_E, -val));
 	}
 	return ret;
+}
+
+namespace detail
+{
+/// Traverses TensorForm extracting data from it.
+template <typename TensorDataType>
+class TensorFormVisitor
+{
+public:
+	TensorFormVisitor() = default;
+
+	std::vector<TensorDataType> operator()(const TensorDataType& singleValue)
+	{
+		return {singleValue};
+	}
+
+	std::vector<TensorDataType> operator()(const RawTensorInitList<TensorDataType>& containerValue)
+	{
+
+		if(collectedShapeIndices_.find(currentLevel) == collectedShapeIndices_.end())
+		{
+			collectedShapeIndices_.emplace(currentLevel, containerValue.size());
+		}
+		else if(containerValue.size() != collectedShapeIndices_.at(currentLevel))
+		{
+			LOG_ERROR("TensorOperations", fmt::format("Inconsistent elements number at axis {}.", currentLevel));
+		}
+
+		std::vector<std::vector<TensorDataType>> collectedValueSets;
+		collectedValueSets.reserve(containerValue.size());
+
+		currentLevel++;
+
+		for(const auto& valueSet : containerValue)
+		{
+			collectedValueSets.emplace_back(std::visit(*this, valueSet));
+		}
+
+		currentLevel--;
+
+		if(collectedValueSets.empty())
+		{
+			LOG_ERROR("TensorOperations", "Encountered empty initializer list at a certain level of raw tensor form.");
+		}
+
+		const auto nElementsInSubValue = collectedValueSets.cbegin()->size();
+
+		std::vector<TensorDataType> collectedValues;
+		collectedValues.reserve(collectedValueSets.size() * nElementsInSubValue);
+
+		for(const auto& valueSet : collectedValueSets)
+		{
+			if(valueSet.size() != nElementsInSubValue)
+			{
+				LOG_ERROR("TensorOperations",
+						  "Encountered not-constant number of subelements at a certain level of raw tensor form.");
+			}
+
+			for(const auto& value : valueSet)
+			{
+				collectedValues.push_back(value);
+			}
+		}
+
+		return collectedValues;
+	}
+
+	std::vector<size_t> getShape() const
+	{
+		std::vector<size_t> collectedShape;
+
+		for(const auto& [axis, index] : collectedShapeIndices_)
+		{
+			collectedShape.push_back(index);
+		}
+
+		return collectedShape;
+	}
+
+private:
+	std::map<size_t, size_t> collectedShapeIndices_{};
+	size_t currentLevel = 0;
+};
+} // namespace detail
+
+template <typename ValueType>
+BasicTensor<ValueType> BasicTensorOperations<ValueType>::makeTensor(const TensorForm<ValueType>& tensorForm)
+{
+	detail::TensorFormVisitor<ValueType> visitor;
+
+	const auto collectedValues = std::visit(visitor, tensorForm);
+	const auto shape = visitor.getShape();
+
+	mlCore::BasicTensor<ValueType> tensor(shape);
+
+	tensor.fill(collectedValues.begin(), collectedValues.end());
+
+	return tensor;
 }
 } // namespace mlCore
