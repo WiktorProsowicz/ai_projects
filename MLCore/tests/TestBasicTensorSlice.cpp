da199f25d7d161f75da9fc8a5a2a67b8372ed423
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-02-09 21:20:13 +0100

    Introduce tensor slice tests

---
+++ MLCore/tests/TestBasicTensorSlice.cpp
@@ -0,0 +1,35 @@
+/**********************
+ * Test suite for 'ai_projects'
+ * 
+ * Copyright (c) 2023
+ * 
+ * by Wiktor Prosowicz
+ **********************/
+
+// __Tested headers__
+#include <MLCore/BasicTensorSlice.h>
+
+// __External software headers__
+#include <gtest/gtest.h>
+
+namespace
+{
+
+/*****************************
+ * 
+ * Test Fixture
+ * 
+ *****************************/
+
+class TestBasicTensorSlice : public testing::Test
+{
+protected:
+};
+
+/*****************************
+ * 
+ * Particular test calls
+ * 
+ *****************************/
+
+} // namespace
