1467975c903def8f17a71a891bd495f334faf463
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-03-05 22:43:31 +0100

    Clean tensor slice interface

--- MLCore/include/MLCore/BasicTensorSlice.h
+++ MLCore/include/MLCore/BasicTensorSlice.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <cstdio>
 #include <functional>
+#include <span>
 
 namespace mlCore
 {
@@ -74,96 +75,23 @@ public:
 	void assignMultiply(const BasicTensorSlice& other);
 
 	/**
-	 * @brief Assigns `value` to all of the elements spanned by the slice.
+	 * @brief Assigns the data from the span to the elements spanned by the slice.
 	 * 
-	 * @param value Value to assign.
+	 * @param data View over the data to be assigned.
 	 */
-	void assign(ValueType value);
+	void assign(const std::span<ValueType>& data);
 
-	/// @see BasicTensorSlice::assign(ValueType)
-	void assignAdd(ValueType value);
+	/// @see BasicTensorSlice::assign(const std::span<ValueType>&)
+	void assignAdd(const std::span<ValueType>& data);
 
-	/// @see BasicTensorSlice::assign(ValueType)
-	void assignSubtract(ValueType value);
+	/// @see BasicTensorSlice::assign(const std::span<ValueType>&)
+	void assignSubtract(const std::span<ValueType>& data);
 
-	/// @see BasicTensorSlice::assign(ValueType)
-	void assignDivide(ValueType value);
+	/// @see BasicTensorSlice::assign(const std::span<ValueType>&)
+	void assignDivide(const std::span<ValueType>& data);
 
-	/// @see BasicTensorSlice::assign(ValueType)
-	void assignMultiply(ValueType value);
-
-	/**
-	 * @brief Assigns given `values` to the elements spanned by the slice.
-	 * 
-	 * @param values Values to assign. Number of elements spanned by the slice should be divisible by the size of `values`.
-	 */
-	void assign(std::initializer_list<ValueType> values);
-
-	/// @see BasicTensorSlice::assign(std::initializer_list<ValueType>)
-	void assignAdd(std::initializer_list<ValueType> values);
-
-	/// @see BasicTensorSlice::assign(std::initializer_list<ValueType>)
-	void assignSubtract(std::initializer_list<ValueType> values);
-
-	/// @see BasicTensorSlice::assign(std::initializer_list<ValueType>)
-	void assignDivide(std::initializer_list<ValueType> values);
-
-	/// @see BasicTensorSlice::assign(std::initializer_list<ValueType>)
-	void assignMultiply(std::initializer_list<ValueType> values);
-
-	/**
-	 * @brief Assigns elements between `begin` and `end` to the spanned elements.
-	 * 
-	 * Number of elements spanned by the slice should be divisible by the distance
-	 * between `begin` and `end`, so that the assigned item set can be wrapped.
-	 * 
-	 * @param begin Begin iterator of the elements to assign.
-	 * @param end End iterator of the elements to assign.
-	 */
-	template <typename InputIter>
-	void assign(InputIter begin, InputIter end) requires std::is_same_v<ValueType, std::iter_value_t<InputIter>>
-	{
-		_assignContiguousData(std::vector<ValueType>(begin, end));
-	}
-
-	/// @see BasicTensorSlice::assign(InputIter, InputIter)
-	template <typename InputIter>
-	void assignAdd(InputIter begin, InputIter end) requires std::is_same_v<ValueType, std::iter_value_t<InputIter>>
-	{
-		_assignAddContiguousData(std::vector<ValueType>(begin, end));
-	}
-
-	/// @see BasicTensorSlice::assign(InputIter, InputIter)
-	template <typename InputIter>
-	void assignSubtract(InputIter begin, InputIter end) requires std::is_same_v<ValueType, std::iter_value_t<InputIter>>
-	{
-		_assignSubtractContiguousData(std::vector<ValueType>(begin, end));
-	}
-
-	/// @see BasicTensorSlice::assign(InputIter, InputIter)
-	template <typename InputIter>
-	void assignDivide(InputIter begin, InputIter end) requires std::is_same_v<ValueType, std::iter_value_t<InputIter>>
-	{
-		_assignDivideContiguousData(std::vector<ValueType>(begin, end));
-	}
-
-	/// @see BasicTensorSlice::assign(InputIter, InputIter)
-	template <typename InputIter>
-	void assignMultiply(InputIter begin, InputIter end) requires std::is_same_v<ValueType, std::iter_value_t<InputIter>>
-	{
-		_assignMultiplyContiguousData(std::vector<ValueType>(begin, end));
-	}
-
-	/**
-	 * @brief Performs matrix multiplication between elements spanned by this and the `other`. The result is assigned to tensor associated to `this`.
-	 * 
-	 * Spanned elements have to conform to the rules applying to the shapes of multiplied matrix.
-	 * Additionally, if the elements of the `other` need to be broadcasted, the shapes should be also
-	 * compatible. Last two dimensions of the matmul result's shape should be the same as the last two dimensions of the `this`'s span shape.
-	 * 
-	 * @param other Tensor slice to multiply the spanned elements by.
-	 */
-	void assignMatmul(const BasicTensorSlice& other);
+	/// @see BasicTensorSlice::assign(const std::span<ValueType>&)
+	void assignMultiply(const std::span<ValueType>& data);
 
 private:
 	/**
@@ -171,29 +99,10 @@ private:
 	 * 
 	 * @param associatedTensor Tensor to be referenced.
 	 * @param indices Span indicating the shape of the slice. The length of the indices should be the same
-     * as the length of the source tensor's shape.
+     * as the length of the source tensor's shape. Each pair of indices should be a range rather than a single index,
+	 * i.e. (2, 3) instead of (2, 2).
 	 */
 	BasicTensorSlice(BasicTensor<ValueType>& associatedTensor, const std::vector<std::pair<size_t, size_t>>& indices);
-
-private:
-	/**
-	 * @brief Assigns all of the elements from `data` to the referenced span of the connected tensor.
-	 * 
-	 * @param data Vector holding contiguous data to assign. 
-	 */
-	void _assignContiguousData(const std::vector<ValueType>& data);
-
-	/// @see BasicTensor::_assignContiguousData(const std::vector<ValueType>)
-	void _assignAddContiguousData(const std::vector<ValueType>& data);
-
-	/// @see BasicTensor::_assignContiguousData(const std::vector<ValueType>)
-	void _assignSubtractContiguousData(const std::vector<ValueType>& data);
-
-	/// @see BasicTensor::_assignContiguousData(const std::vector<ValueType>)
-	void _assignDivideContiguousData(const std::vector<ValueType>& data);
-
-	/// @see BasicTensor::_assignContiguousData(const std::vector<ValueType>)
-	void _assignMultiplyContiguousData(const std::vector<ValueType>& data);
 
 	/// Computes the slice's shape based on its indices and the shape of the associated tensor.
 	std::vector<size_t> _computeSliceShape() const;
@@ -204,7 +113,9 @@ private:
 	/// Computes size of the chunk of contiguous memory spanned by the slice.
 	size_t _computeChunkLength() const;
 
-private:
+	/// Determines pairs of pointers to arrays of data that shall be aligned together after broadcasting.
+	std::vector<std::pair<ValueType*, ValueType*>> _determineBroadcastedDataPointers(const BasicTensorSlice& other) const;
+
 	std::reference_wrapper<mlCore::BasicTensor<ValueType>> tensor_;
 	std::vector<std::pair<size_t, size_t>> indices_;
 };
