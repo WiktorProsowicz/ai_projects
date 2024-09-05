a0b02a6c476794feab52906802dc4b9eebc3a2b4
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-09-05 12:38:12 +0000

    Fix weights serializer

--- cpplibs/Layers/src/Serialization/WeightsSerializer.cpp
+++ cpplibs/Layers/src/Serialization/WeightsSerializer.cpp
@@ -18,8 +18,8 @@ std::unique_ptr<WeightsSerializer> WeightsSerializer::open(const std::string& pa
 		_validateFile(path);
 	}
 
-	std::unique_ptr<std::fstream> fileStream = std::make_unique<std::fstream>(
-		path, std::ios_base::app | std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+	std::unique_ptr<std::fstream> fileStream =
+		std::make_unique<std::fstream>(path, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
 
 	return std::unique_ptr<WeightsSerializer>{new WeightsSerializer{std::move(fileStream)}};
 }
@@ -33,7 +33,7 @@ WeightsSerializer::WeightsSerializer(std::unique_ptr<std::fstream> fileStream)
 namespace
 {
 /// Returns the number of tensors stored inside the analyzed file.
-size_t getNBlocks(std::istream& file)
+uint64_t getNBlocks(std::istream& file)
 {
 	const auto initPos = file.tellg();
 	size_t nBlocks;
@@ -48,89 +48,98 @@ size_t getNBlocks(std::istream& file)
 
 void WeightsSerializer::addNewTensor(const mlCore::Tensor& tensor)
 {
-	_fileStream->seekp(0, std::ios_base::beg);
-
-	auto nBlocks = getNBlocks(*_fileStream);
+	uint64_t nBlocks = getNBlocks(*_fileStream);
 	nBlocks++;
 
-	_fileStream->write(reinterpret_cast<char*>(&nBlocks), sizeof(nBlocks));
+	_fileStream->seekp(0, std::ios_base::beg);
+	(*_fileStream) << utilities::SerializationPack(nBlocks);
 
 	_fileStream->seekp(0, std::ios_base::end);
-
 	(*_fileStream) << utilities::SerializationPack(size_t{tensor.shape().size()});
 	(*_fileStream) << utilities::SerializationPack(tensor.shape());
-	std::copy(tensor.begin(), tensor.end(), std::ostream_iterator<char>(*_fileStream));
 
+	for(const auto value : tensor)
+	{
+		_fileStream->write(reinterpret_cast<const char*>(&value), sizeof(double));
+	}
+
+	_fileStream->flush();
 	_tensorHandles.emplace_back(std::make_shared<TensorHandle>(*_fileStream, _fileStream->tellp()));
 }
 
 void WeightsSerializer::_validateFile(const std::string& path)
 {
-	if(std::filesystem::file_size(path) < 8)
+	if(std::filesystem::file_size(path) < sizeof(uint64_t))
 	{
-		LOG_ERROR("Layers::WeightsSerializer", "File is too small to be a valid weights file.");
+		LOG_ERROR("Layers::WeightsSerializer",
+				  fmt::format("File '{}' is too small to be a valid weights file.", path));
 	}
 
 	std::ifstream fileStream(path);
 
 	const auto fileEnd = fileStream.seekg(0, std::ios_base::end).tellg();
-	std::streampos filePos = fileStream.seekg(8, std::ios_base::beg).tellg();
+	std::streampos filePos = fileStream.seekg(sizeof(uint64_t), std::ios_base::beg).tellg();
 
 	size_t nBlocks = getNBlocks(fileStream);
 
 	for(size_t blockIdx = 0; blockIdx < nBlocks; blockIdx++)
 	{
-		if((fileEnd - filePos) < 8)
+		if(static_cast<size_t>(fileEnd - filePos) < sizeof(size_t))
 		{
 			LOG_ERROR("Layers::WeightsSerializer",
-					  fmt::format("For block {} could not read the number of dimensions.", blockIdx));
+					  fmt::format("For block {} in file '{}' could not read the number of dimensions.",
+								  blockIdx,
+								  path));
 		}
 
 		size_t nDimensions;
 		fileStream.readsome(reinterpret_cast<char*>(&nDimensions), sizeof(size_t));
-		filePos += 8;
+		filePos += sizeof(uint64_t);
 		fileStream.seekg(filePos);
 
-		if(static_cast<size_t>(fileEnd - filePos) < 8 * nDimensions)
+		if(static_cast<size_t>(fileEnd - filePos) < (sizeof(size_t) * nDimensions))
 		{
-			LOG_ERROR("Layers::WeightsSerializer",
-					  fmt::format("For block {} not enough data to read dimensions.", blockIdx));
+			LOG_ERROR(
+				"Layers::WeightsSerializer",
+				fmt::format("For block {} in file '{}' not enough data to read dimensions.", blockIdx, path));
 		}
 
 		std::vector<size_t> dimensions(nDimensions);
-		fileStream.readsome(reinterpret_cast<char*>(dimensions.data()), 8 * nDimensions);
-		filePos += 8 * nDimensions;
+		fileStream.readsome(reinterpret_cast<char*>(dimensions.data()), sizeof(size_t) * nDimensions);
+		filePos += sizeof(size_t) * nDimensions;
 		fileStream.seekg(filePos);
 
 		const auto tensorSize =
 			std::accumulate(dimensions.begin(), dimensions.end(), size_t{1}, std::multiplies<>());
 
-		if(static_cast<size_t>(fileEnd - filePos) < 8 * tensorSize)
+		if(static_cast<size_t>(fileEnd - filePos) < sizeof(double) * tensorSize)
 		{
 			LOG_ERROR("Layers::WeightsSerializer",
-					  fmt::format("For block {} not enough data to read tensor data.", blockIdx));
+					  fmt::format(
+						  "For block {} in file '{}' not enough data to read tensor data.", blockIdx, path));
 		}
 
-		filePos += 8 * tensorSize;
+		filePos += sizeof(double) * tensorSize;
 		fileStream.seekg(filePos);
 	}
 }
 
 void WeightsSerializer::_initHandles()
 {
-	std::streampos currentPos = 8;
-	const auto fileEnd = _fileStream->seekg(0, std::ios_base::end).tellg();
+	std::streampos currentPos = sizeof(uint64_t);
 
-	while(currentPos < fileEnd)
+	for(size_t blockIdx = 0; blockIdx < getNBlocks(*_fileStream); blockIdx++)
 	{
 		const auto& newestHandle =
 			_tensorHandles.emplace_back(std::make_shared<TensorHandle>(*_fileStream, currentPos));
 
 		const auto newestHandleShape = newestHandle->getShape();
 
-		currentPos += 8 + newestHandleShape.size() * 8;
-		currentPos += std::accumulate(
-			newestHandleShape.cbegin(), newestHandleShape.cend(), size_t{8}, std::multiplies<>());
+		currentPos += sizeof(size_t) + newestHandleShape.size() * sizeof(size_t);
+		currentPos += std::accumulate(newestHandleShape.cbegin(),
+									  newestHandleShape.cend(),
+									  size_t{sizeof(double)},
+									  std::multiplies<>());
 	}
 }
 
@@ -154,12 +163,18 @@ void TensorHandle::save(const mlCore::Tensor& tensor)
 							  tensor.size()));
 	}
 
-	_file.seekp(_position + static_cast<std::streamoff>(8));
-	_file.write(reinterpret_cast<const char*>(tensor.shape().data()), 8 * tensor.shape().size());
+	_file.seekp(_position + static_cast<std::streamoff>(sizeof(size_t)));
+	_file << utilities::SerializationPack(tensor.shape());
 
-	_file.seekp(_position + static_cast<std::streamoff>(8 + 8 * tensor.shape().size()));
+	_file.seekp(_position +
+				static_cast<std::streamoff>(sizeof(size_t) + sizeof(size_t) * tensor.shape().size()));
 
-	std::copy(tensor.begin(), tensor.end(), std::ostream_iterator<double>(_file));
+	for(const auto value : tensor)
+	{
+		_file.write(reinterpret_cast<const char*>(&value), sizeof(double));
+	}
+
+	_file.flush();
 }
 
 mlCore::Tensor TensorHandle::get() const
@@ -167,9 +182,12 @@ mlCore::Tensor TensorHandle::get() const
 	const auto shape = getShape();
 	mlCore::Tensor tensor(shape);
 
-	_file.seekg(_position + static_cast<std::streamoff>(8 + 8 * shape.size()));
+	_file.seekg(_position + static_cast<std::streamoff>(sizeof(size_t) + sizeof(size_t) * shape.size()));
 
-	std::copy_n(std::istream_iterator<double>(_file), tensor.size(), tensor.begin());
+	for(auto& value : tensor)
+	{
+		_file.read(reinterpret_cast<char*>(&value), sizeof(double));
+	}
 
 	return tensor;
 }
@@ -179,10 +197,10 @@ mlCore::TensorShape TensorHandle::getShape() const
 	size_t nDimensions;
 	_file.seekg(_position);
 	_file.readsome(reinterpret_cast<char*>(&nDimensions), sizeof(size_t));
-	_file.seekg(_position + static_cast<std::streamoff>(8));
+	_file.seekg(_position + static_cast<std::streamoff>(sizeof(size_t)));
 
 	std::vector<size_t> dimensions(nDimensions);
-	_file.readsome(reinterpret_cast<char*>(dimensions.data()), 8 * nDimensions);
+	_file.readsome(reinterpret_cast<char*>(dimensions.data()), sizeof(size_t) * nDimensions);
 
 	return dimensions;
 }
